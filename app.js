1b144b888e45270f3ad3d1108ff954e04a72e36a
AslanSoft <beggyplayy@gmail.com>
2025-07-16 23:52:39 +0500

    js added

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-let button2 = document.querySelector('.button')
+let button2 = document.querySelector(".button2")
+let loginpage = document.querySelector(".loginpage")
+let loginbutton2 = document.querySelector(".loginbutton2")
+let param = true
+button2.addEventListener('click', (event) => {
+    param =! param
+    if(param) {
+        loginpage.style.display="none"
+    }else{
+        loginpage.style.display="flex"
+    }
+});
+
+loginbutton2.addEventListener('click', () =>{
+    loginpage.style.display="none"
+    param = true
+});
