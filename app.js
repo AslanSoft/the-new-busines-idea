fd93da0d30363a68308b08512d6d8305468906b4
AslanSoft <beggyplayy@gmail.com>
2025-07-22 18:26:33 +0500

    animation added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let button2 = document.querySelector(".button2")
-let loginpage = document.querySelector(".loginpage")
+let loginpage = document.querySelector(".overview")
 let loginbutton2 = document.querySelector(".loginbutton2")
 let param = true
 button2.addEventListener('click', (event) => {
+    if(loginpage.classList.contains("remove-login")){
+    loginpage.classList.remove("remove-login")
+}
     param =! param
     if(param) {
         loginpage.style.display="none"
@@ -12,6 +15,10 @@ button2.addEventListener('click', (event) => {
 });
 
 loginbutton2.addEventListener('click', () =>{
-    loginpage.style.display="none"
+    loginpage.classList.add("remove-login")
     param = true
+    console.log(loginpage)
 });
+
+
+
